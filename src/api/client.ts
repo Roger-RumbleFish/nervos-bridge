f0616ff88dd7bd28ba8f9ecbb823d3c82947481b
Rskrzypczyk <roger.skrzypczyk@rumblefish.pl>
2021-10-01 13:44:39 +0200

    update api url

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,8 @@
 import { BridgeRPCHandler } from 'nervos-godwoken-integration'
 
 export const getBridgeRPCClient = (url?: string): BridgeRPCHandler => {
-  const forceBridgeUrl = url ?? 'https://bridge-godwoken.rumblefish.dev/'
+  const forceBridgeUrl =
+    url ?? 'https://testnet.force-bridge.com/api/force-bridge/api/v1'
   const rpcClient = new BridgeRPCHandler(forceBridgeUrl)
   return rpcClient
 }
