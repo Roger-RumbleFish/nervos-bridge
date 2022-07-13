6691d825ccf473d82295e295fe65ab8476045a2d
Patryk Wadowski <patryk.wadowski@rumblefish.pl>
2022-07-13 12:52:05 +0200

    markers cleanup

--- src/api/registry/godwoken/godwokenRegistry.mainnet.ts
+++ src/api/registry/godwoken/godwokenRegistry.mainnet.ts
@@ -3,55 +3,55 @@ import { Network } from '@interfaces/data'
 
 export const registry: TokensRegistry = {
   [CanonicalTokenSymbol.CKB]: {
-    address: '0x7538C85caE4E4673253fFd2568c1F1b48A71558a', //
+    address: '0x7538C85caE4E4673253fFd2568c1F1b48A71558a',
     symbol: CanonicalTokenSymbol.CKB,
     decimals: 8,
     network: Network.CKB,
   },
   [CanonicalTokenSymbol.dCKB]: {
-    address: '0x893474456C475E738b13DdA824979bF7a355DE39', //
+    address: '0x893474456C475E738b13DdA824979bF7a355DE39',
     symbol: CanonicalTokenSymbol.dCKB,
     decimals: 8,
     network: Network.CKB,
   },
   [CanonicalTokenSymbol.DAI]: {
-    address: '0x2c9Fc6087875646112f66a3C92fEF2d158FAa76e', //
+    address: '0x2c9Fc6087875646112f66a3C92fEF2d158FAa76e',
     symbol: CanonicalTokenSymbol.DAI,
     decimals: 18,
     network: Network.Ethereum,
   },
   [CanonicalTokenSymbol.USDC]: {
-    address: '0x186181e225dc1Ad85a4A94164232bD261e351C33', //
+    address: '0x186181e225dc1Ad85a4A94164232bD261e351C33',
     symbol: CanonicalTokenSymbol.USDC,
     decimals: 6,
     network: Network.Ethereum,
   },
   [CanonicalTokenSymbol.USDT]: {
-    address: '0x8E019acb11C7d17c26D334901fA2ac41C1f44d50', //
+    address: '0x8E019acb11C7d17c26D334901fA2ac41C1f44d50',
     symbol: CanonicalTokenSymbol.USDT,
     decimals: 6,
     network: Network.Ethereum,
   },
   [CanonicalTokenSymbol.BNBBSC]: {
-    address: '0xBAdb9b25150Ee75bb794198658A4D0448e43E528', //
+    address: '0xBAdb9b25150Ee75bb794198658A4D0448e43E528',
     symbol: CanonicalTokenSymbol.BNBBSC,
     decimals: 18,
     network: Network.BSC,
   },
   [CanonicalTokenSymbol.USDTBSC]: {
-    address: '0xDFF2faCdFe47C1D5b51f18231f900949F1d5988f', //
+    address: '0xDFF2faCdFe47C1D5b51f18231f900949F1d5988f',
     symbol: CanonicalTokenSymbol.USDTBSC,
     decimals: 18,
     network: Network.BSC,
   },
   [CanonicalTokenSymbol.USDCBSC]: {
-    address: '0xfA307CfdEA89DC197A346c338a98aC85d517af6e', //
+    address: '0xfA307CfdEA89DC197A346c338a98aC85d517af6e',
     symbol: CanonicalTokenSymbol.USDCBSC,
     decimals: 18,
     network: Network.BSC,
   },
   [CanonicalTokenSymbol.BUSDBSC]: {
-    address: '0xD07920d57F400D89d62535D50eb9D1200ed7821B', //
+    address: '0xD07920d57F400D89d62535D50eb9D1200ed7821B',
     symbol: CanonicalTokenSymbol.BUSDBSC,
     decimals: 18,
     network: Network.BSC,
